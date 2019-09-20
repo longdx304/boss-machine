6f874d180891086d6f9c0e0a74e0d41bcf76528e
Long Do <longdo@Longs-MacBook-Air-2.local>
2019-09-20 14:40:55 +0700

    check if idea worth 1 mil dollar server-side

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,4 +1,12 @@
-const checkMillionDollarIdea = () => {};
+const checkMillionDollarIdea = (req, res, next) => {
+    const ideaValue = req.body.weeklyRevenue * req.body.numWeeks; //calculate idea value
+    if (ideaValue < 1000000) { //if not worth at least 1 mil
+        const err = new Error('Idea NOT worth 1 Mil dollars');
+        err.status = 400;
+        return next(err); //return error 
+    }
+    next();
+};
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
--- server/routes/ideas.js
+++ server/routes/ideas.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const ideasRouter = express.Router();
 
+const checkMillionDollarIdea = require('../checkMillionDollarIdea');
 const {deleteFromDatabasebyId, getAllFromDatabase, addToDatabase, getFromDatabaseById, updateInstanceInDatabase} = require('../db'); //import database's function
 
 ideasRouter.param('ideaId', (req, res, next, id) => { //check obj ID and attach obj to request
@@ -18,7 +19,7 @@ ideasRouter.get('/', (req, res, next) => { //Route for GET request
     res.send(ideas); //send back respond with data
 });
 
-ideasRouter.post('/', (req, res, next) => { //Route for POST Request to create new obj and save to db
+ideasRouter.post('/', checkMillionDollarIdea, (req, res, next) => { //Route for POST Request to create new obj and save to db
     const idea = addToDatabase('ideas' ,req.body); //use addToDatabase(name, obj from req.body) to add obj and store return value
     res.status(201).send(idea); //send 201 CREATED status
 });
@@ -27,7 +28,7 @@ ideasRouter.get('/:ideaId', (req, res, next) => { //Route for GET request to ret
     res.status(200).send(req.idea); //send res status 200 with obj instance
 });
 
-ideasRouter.put('/:ideaId', (req, res, next) => { //Route for PUT request to update single obj by id
+ideasRouter.put('/:ideaId', checkMillionDollarIdea, (req, res, next) => { //Route for PUT request to update single obj by id
     const idea = updateInstanceInDatabase('ideas', req.body); //update db using updateInstanceInDatabase(model, obj from req.body)
     res.status(200).send(idea); //send res status 200 with updated obj
 });
