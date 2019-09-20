9cbbcdfccf6ea952b203984fbe5304b998ffc23a
Long Do <longdo@Longs-MacBook-Air-2.local>
2019-09-20 13:12:59 +0700

    add route to get info of single minion or idea

--- server/routes/ideas.js
+++ server/routes/ideas.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const ideasRouter = express.Router();
 
-const {getAllFromDatabase, addToDatabase} = require('../db'); //import database's function
+const {getAllFromDatabase, addToDatabase, getFromDatabaseById} = require('../db'); //import database's function
 
 ideasRouter.get('/', (req, res, next) => { //Route for GET request
     const ideas = getAllFromDatabase('ideas'); //retrieve array of data from db
@@ -11,6 +11,16 @@ ideasRouter.get('/', (req, res, next) => { //Route for GET request
 ideasRouter.post('/', (req, res, next) => { //Route for POST Request to create new obj and save to db
     const idea = addToDatabase('ideas' ,req.body); //use addToDatabase(name, obj from req.body) to add obj and store return value
     res.status(201).send(idea); //send 201 CREATED status
+});
+
+ideasRouter.get('/:ideaId', (req, res, next) => { //Route for GET request to retrieve info for one obj
+    const idea = getFromDatabaseById('ideas', req.params.ideaId); //retrieve minion using getFromDatabaseById(model, id from params.minionId)
+    if (!idea) { //if retrieved obj is null
+        const err = new Error('Invalid ID requested');
+        err.status = 400; //create error
+        return next(err); //return error
+    }
+    res.status(200).send(idea); //send res status 200 with obj instance
 });
 
 module.exports = ideasRouter;
--- server/routes/minions.js
+++ server/routes/minions.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const minionsRouter = express.Router();
 
-const {getAllFromDatabase, addToDatabase} = require('../db'); //import database's function
+const {getAllFromDatabase, addToDatabase, getFromDatabaseById} = require('../db'); //import database's function
 
-minionsRouter.get('/', (req, res, next) => { //Route for GET request
+minionsRouter.get('/', (req, res, next) => { //Route for GET request to retrieve info for all objs
     const minions = getAllFromDatabase('minions'); //retrieve array of all minions from db and save to minions
     res.send(minions); //send back respond with array of all minions
 });
@@ -13,6 +13,14 @@ minionsRouter.post('/', (req, res, next) => { //Route for POST Request to create
     res.status(201).send(minion); //send 201 CREATED status
 });
 
-
+minionsRouter.get('/:minionId', (req, res, next) => { //Route for GET request to retrieve info for one obj
+    const minion = getFromDatabaseById('minions', req.params.minionId); //retrieve minion using getFromDatabaseById(model, id from params.minionId)
+    if (!minion) { //if retrieved obj is null
+        const err = new Error('Invalid ID requested');
+        err.status = 400; //create error
+        return next(err); //return error
+    }
+    res.status(200).send(minion); //send res status 200 with obj instance
+});
 
 module.exports = minionsRouter;
