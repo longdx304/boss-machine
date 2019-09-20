c98807dd88c69ac4e32d4778ae6b72dbcbc5d1ad
Long Do <longdo@Longs-MacBook-Air-2.local>
2019-09-20 14:25:37 +0700

    add delete route to delete all meetings

--- server/routes/meetings.js
+++ server/routes/meetings.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const meetingsRouter = express.Router();
 
-const {getAllFromDatabase, createMeeting, addToDatabase} = require('../db'); //import database's function
+const {deleteAllFromDatabase, getAllFromDatabase, createMeeting, addToDatabase} = require('../db'); //import database's function
 
 meetingsRouter.get('/', (req, res, next) => { //Route for GET request
     const meetings = getAllFromDatabase('meetings'); //retrieve array of data from db
@@ -11,6 +11,11 @@ meetingsRouter.get('/', (req, res, next) => { //Route for GET request
 meetingsRouter.post('/', (req, res, next) => { //Route for POST Request to create new obj and save to db
     const meeting = addToDatabase('meetings', createMeeting()); //use addToDatabase(name, obj from db createMeeting()) to add obj and store return value
     res.status(201).send(meeting); //send 201 CREATED status
+});
+
+meetingsRouter.delete('/', (req, res, next) => { //Route for DELETE request to delete all meeting
+    deleteAllFromDatabase('meetings'); //use deleteAllFromDatabase(model)
+    res.status(204).send(); //send 204 status
 });
 
 module.exports = meetingsRouter;
